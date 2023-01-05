caeffafe4940f165aaf33310a7175e542e15e0fd
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2023-01-05 11:25:21 +0300

    #1 #5 add tests for class Gameloop, up maintenance version

--- tests/gameloop.test.js
+++ tests/gameloop.test.js
@@ -73,3 +73,45 @@ describe('[Class][Gameloop] call stop after call start', () => {
     expect(gameloop.isGameOver).toBe(false);
   });
 });
+
+describe('[Class][Gameloop] call setGameOver after call start', () => {
+  const params = {
+    speedRate: 1000,
+  }
+  const gameloop = new Gameloop(params);
+  gameloop.start();
+  gameloop.setGameOver();
+
+  test('After call setGameOver interval not null', () => {
+    expect(gameloop.interval).not.toBeNull();
+  });
+
+  test('After call setGameOver isPaused is true', () => {
+    expect(gameloop.isPaused).toBe(true);
+  });
+
+  test('After call setGameOver isGameOver is true', () => {
+    expect(gameloop.isGameOver).toBe(true);
+  });
+});
+
+describe('[Class][Gameloop] call clear after call start', () => {
+  const params = {
+    speedRate: 1000,
+  }
+  const gameloop = new Gameloop(params);
+  gameloop.start();
+  gameloop.clear();
+
+  test('After call clear interval is null', () => {
+    expect(gameloop.interval).toBeNull();
+  });
+
+  test('After call clear isPaused is false', () => {
+    expect(gameloop.isPaused).toBe(false);
+  });
+
+  test('After call clear isGameOver is false', () => {
+    expect(gameloop.isGameOver).toBe(false);
+  });
+});
