0644194915001efd80bcc4cc45e0598516164400
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2023-01-05 11:00:22 +0300

    #1 #5 add class Gameloop, up minor version, new build

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
-﻿
+﻿'use strict';
+
+export class Gameloop {
+  constructor(params) {
+    this.params = params;
+    this.#init();
+  }
+
+  start() {
+    if (this.isGameOver) {
+      this.clear();
+    }
+
+    this.isPaused = false;
+    this.interval = setInterval(this.#eventLoop.bind(this), this.SPEED_RATE);
+  }
+
+  stop() {
+    this.isPaused = true;
+    clearTimeout(this.interval);
+  }
+
+  setGameOver() {
+    this.stop();
+    this.isGameOver = true;
+  }
+
+  clear() {
+    this.stop();
+    this.#init();
+  }
+
+  #init() {
+    this.interval = null;
+    this.isGameOver = false;
+    this.isPaused = false;
+
+    this.SPEED_RATE = this.params.speedRate;
+  }
+
+  #eventLoop() {
+    this.#update();
+    this.#render();
+    this.#draw();
+    this.#eventHandler();
+  }
+
+  #update() { }
+
+  #render() { }
+
+  #draw() { }
+
+  #eventHandler() { }
+}
+
+export default Gameloop;
