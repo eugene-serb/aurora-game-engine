2d6d66cdd7001d5864109da7c220d0f4905a346b
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2023-01-07 12:21:08 +0300

    #1 #11 add class keyboard

---
+++ src/keyboard.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+export class Keyboard {
+  constructor(target, context) {
+    this._target = target;
+    this._context = context;
+    this.#handle();
+  }
+
+  #handle() { }
+}
+
+export default Keyboard;
