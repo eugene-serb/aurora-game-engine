2991203b17e71f4bf05d9e584fdee6839cca6f9a
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2023-01-22 14:58:57 +0300

    #35 update module keyboard

--- src/keyboard.js
+++ src/keyboard.js
@@ -6,7 +6,36 @@ export class Keyboard {
     this.#handle();
   }
 
-  #handle() { }
+  #handle() {
+    window.addEventListener('keydown', (e) => {
+      if (!this._context.isGameOver) {
+        if (e.code === 'ArrowLeft' || e.code === 'KeyA') {
+          this._context.moveToLeft();
+        } else if (e.code === 'ArrowUp' || e.code === 'KeyW') {
+          this._context.moveToUp();
+        } else if (e.code === 'ArrowRight' || e.code === 'KeyD') {
+          this._context.moveToRight();
+        } else if (e.code === 'ArrowDown' || e.code === 'KeyS') {
+          this._context.moveToDown();
+        }
+      }
+
+      if (e.code === 'KeyR') {
+        this._context.clear();
+        this._context.start();
+      }
+
+      if (e.code === 'KeyP') {
+        if (!this._context.isGameOver) {
+          if (this._context.isPaused) {
+            this._context.start();
+          } else {
+            this._context.stop();
+          }
+        }
+      }
+    });
+  }
 }
 
 export default Keyboard;
